2c1c4aab7a65971b327f77fb9b0f3045f745dffb
sarybe <sarybe@gmail.com>
2014-03-11 18:16:02 +0000

    value1 -> tn_name_link Réécriture des variables tn_type

--- language/fr_FR/plugin.lang.php
+++ language/fr_FR/plugin.lang.php
@@ -1,22 +1,22 @@
-<?php
+﻿<?php
 $lang['tn_display'] = 'Affichage';
 $lang['tn_display_name'] = 'Afficher le titre de l\'image';
+$lang['tn_name_link'] = 'Type d\'affichage du titre';
 $lang['tn_separator'] = 'Separateur (-)';
 $lang['tn_none'] = 'Aucun';
 $lang['tn_yes'] = 'Oui';
 $lang['tn_no'] = 'Non';
 
-$lang['tn_type1'] = '{title}';
-$lang['tn_type2'] = '{nb_visits}';
-$lang['tn_type3'] = '({nb_visits})';
-$lang['tn_type4'] = '{description}';
-$lang['tn_type5'] = '{Author}';
-$lang['tn_type6'] = 'Auteur : {Author}';
-$lang['tn_type7'] = '{rating_score}';
-$lang['tn_type8'] = '({nb_visits}, {rating_score})';
+$lang['tn_type1'] = '{Titre}';
+$lang['tn_type2'] = '{nb} visites';
+$lang['tn_type3'] = '({nb} visites)';
+$lang['tn_type4'] = '{Description}';
+$lang['tn_type5'] = '{Auteur}';
+$lang['tn_type6'] = 'Auteur : {Auteur}';
+$lang['tn_type7'] = 'Score {note moyenne} ';
+$lang['tn_type8'] = '({nb} visites, score {note moyenne})';
 
 $lang['tn_configuration_saved'] = 'Configuration sauvegardée';
 $lang['tn_default_parameters'] = 'Paramètres par défaut';
 $lang['tn_default_parameters_saved'] = 'Paramètres par défaut sauvegardés';
-$lang['value1'] = 'Type de titre';
 ?>
