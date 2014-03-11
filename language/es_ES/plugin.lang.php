54ce9da2b9b2e7cbe3b68c8f76bf5b4f42082e36
TranslationTeam <translate-github@piwigo.org>
2014-03-11 11:07:22 +0000

    [thumbnailtooltip] Update es_ES, thanks to : jpr928

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['tn_configuration_saved'] = 'Configuración guardada';
+$lang['tn_default_parameters'] = 'Configuración por defecto';
+$lang['tn_default_parameters_saved'] = 'Configuración por defecto guardada';
+$lang['tn_display'] = 'Visualización';
+$lang['tn_display_name'] = 'Visualización del titulo de la imagen';
+$lang['tn_no'] = 'No';
+$lang['tn_none'] = 'Ninguno';
+$lang['tn_separator'] = 'Separador (-)';
+$lang['tn_type1'] = '{titulo}';
+$lang['tn_type2'] = '{nb_visitas}';
+$lang['tn_type3'] = '({nb_visitas})';
+$lang['tn_type4'] = '{descrillpción}';
+$lang['tn_type5'] = '{Autor}';
+$lang['tn_type6'] = 'Auteur : {Autor}';
+$lang['tn_type7'] = '{puntuación}';
+$lang['tn_type8'] = '({nb_visitas}, {puntuación})';
+$lang['tn_yes'] = 'Si';
+$lang['value1'] = 'Tipo de titulo';
+?>
