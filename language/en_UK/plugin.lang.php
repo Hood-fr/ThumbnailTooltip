d2c6ec8cba008779ffc260796e0351b258311807
sarybe <sarybe@gmail.com>
2014-03-12 21:43:35 +0000

    Remove brackets and change some words

--- language/en_UK/plugin.lang.php
+++ language/en_UK/plugin.lang.php
@@ -7,14 +7,14 @@ $lang['tn_none'] = 'None';
 $lang['tn_yes'] = 'Yes';
 $lang['tn_no'] = 'No';
 
-$lang['tn_type1'] = '{title}';
-$lang['tn_type2'] = '{nb_visits}';
-$lang['tn_type3'] = '({nb_visits})';
-$lang['tn_type4'] = '{description}';
-$lang['tn_type5'] = '{Author}';
-$lang['tn_type6'] = 'Author: {Author}';
-$lang['tn_type7'] = '{rating_score}';
-$lang['tn_type8'] = '({nb_visits}, {rating_score})';
+$lang['tn_type1'] = 'Title';
+$lang['tn_type2'] = 'Hits';
+$lang['tn_type3'] = '(Hits)';
+$lang['tn_type4'] = 'Description';
+$lang['tn_type5'] = 'Author';
+$lang['tn_type6'] = 'Author: Author';
+$lang['tn_type7'] = 'Rating score';
+$lang['tn_type8'] = '(Hits, Rating score)';
 
 $lang['tn_configuration_saved'] = 'Configuration saved';
 $lang['tn_default_parameters'] = 'Default configuration';
