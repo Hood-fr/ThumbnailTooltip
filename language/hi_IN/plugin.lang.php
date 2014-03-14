de95c9cda76bda6578d008c012c51ea853e0ee89
sarybe <sarybe@gmail.com>
2014-03-14 08:51:56 +0000

    [thumbnailtooltip] add hi_IN language

---
+++ language/hi_IN/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+$lang['tn_display'] = 'फ़ोटो डिस्प्ले';
+$lang['tn_display_name'] = 'फ़ोटो डिस्प्ले शीर्षक';
+$lang['tn_name_link'] = 'शीर्षक केटेगरी';
+$lang['tn_separator'] = 'विभाजक (-)';
+$lang['tn_none'] = 'कोई नहीं';
+$lang['tn_yes'] = 'हाँ';
+$lang['tn_no'] = 'नहीं';
+
+$lang['tn_type1'] = 'शीर्षक';
+$lang['tn_type2'] = 'हिट';
+$lang['tn_type3'] = '(हिट)';
+$lang['tn_type4'] = 'विवरण';
+$lang['tn_type5'] = 'लेखक';
+$lang['tn_type6'] = 'लेखक: लेखक';
+$lang['tn_type7'] = 'रेटिंग स्कोर';
+$lang['tn_type8'] = '(हिट, रेटिंग स्कोर)';
+
+$lang['tn_configuration_saved'] = 'कॉन्फ़िगरेशन सुरक्षित';
+$lang['tn_default_parameters'] = ' डिफ़ॉल्ट  कॉन्फ़िगरेशन ';
+$lang['tn_default_parameters_saved'] = 'डिफ़ॉल्ट  कॉन्फ़िगरेशन सुरक्षित';
+?>
