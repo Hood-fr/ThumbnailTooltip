0d91ad87f3a77410fd6929dbb9b0d8e2cd07b75a
TranslationTeam <translate-github@piwigo.org>
2014-03-19 05:21:32 +0000

    [thumbnailtooltip] Update zh_CN, thanks to : junwang

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['tn_no'] = '不';
+$lang['tn_none'] = '没有';
+$lang['tn_separator'] = '分离器（-）';
+$lang['tn_type1'] = '标题';
+$lang['tn_type2'] = '点击';
+$lang['tn_type3'] = '(点击)';
+$lang['tn_type4'] = '描述';
+$lang['tn_type5'] = '作者';
+$lang['tn_type6'] = '作者：作者';
+$lang['tn_type7'] = '评分';
+$lang['tn_type8'] = '（点击，评分）';
+$lang['tn_yes'] = '是';
+$lang['tn_display_name'] = '显示图片标题';
+$lang['tn_name_link'] = '标题类型';
+$lang['tn_display'] = '显示';
+$lang['tn_default_parameters_saved'] = '默认配置已保存';
+$lang['tn_default_parameters'] = '默认配置';
+$lang['tn_configuration_saved'] = '配置已保存';
