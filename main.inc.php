f6dc00513e3d21e61d1f13afa46175684dd807ba
sarybe <sarybe@gmail.com>
2014-10-16 07:39:11 +0000

    [Thumbnail_Tooltip] Bug fix for html and double quotes in comment

--- main.inc.php
+++ main.inc.php
@@ -1,25 +1,25 @@
-<?php
-/*
-Plugin Name: Thumbnail Tooltip
-Version: 1.4.5
-Description: Permet de changer l'infobulle sur les images dans les albums.
-Plugin URI: http://piwigo.org/ext/extension_view.php?eid=738
-Author: Sarybe
-Author URI: http://www.lesrempartsdetours.com
-*/
-
-// Inspiré des plugins CatAddFav by jfc (http://fr.piwigo.org/ext/extension_view.php?eid=489) et Lightbox by P@t (http://piwigo.org/ext/extension_view.php?eid=280)
-
-if (!defined('PHPWG_ROOT_PATH')) {
-  die('Hacking attempt!');
-}
-
-define('thumbnail_tooltip_PATH' , PHPWG_PLUGINS_PATH.basename(dirname(__FILE__)).'/');
-
-if (script_basename()  == 'admin') {
-  include(dirname(__FILE__).'/admin/functions.inc.php');
-}
-else {
-  include_once(dirname(__FILE__).'/initialize.inc.php');
-}
+<?php
+/*
+Plugin Name: Thumbnail Tooltip
+Version: 1.4.8
+Description: Permet de changer l'infobulle sur les images dans les albums.
+Plugin URI: http://piwigo.org/ext/extension_view.php?eid=738
+Author: Sarybe
+Author URI: http://www.lesrempartsdetours.com
+*/
+
+// Inspiré des plugins CatAddFav by jfc (http://fr.piwigo.org/ext/extension_view.php?eid=489) et Lightbox by P@t (http://piwigo.org/ext/extension_view.php?eid=280)
+
+if (!defined('PHPWG_ROOT_PATH')) {
+  die('Hacking attempt!');
+}
+
+define('thumbnail_tooltip_PATH' , PHPWG_PLUGINS_PATH.basename(dirname(__FILE__)).'/');
+
+if (script_basename()  == 'admin') {
+  include(dirname(__FILE__).'/admin/functions.inc.php');
+}
+else {
+  include_once(dirname(__FILE__).'/initialize.inc.php');
+}
 ?>
