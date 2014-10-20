e19def41d76801fa56a2d63f856dae5a5d60b23d
sarybe <sarybe@gmail.com>
2014-10-20 12:08:13 +0000

    [Thumbnail_Tooltip] Bug fix for html and double quotes in name (title)

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Thumbnail Tooltip
-Version: 1.4.8
+Version: 1.4.8.1
 Description: Permet de changer l'infobulle sur les images dans les albums.
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=738
 Author: Sarybe
