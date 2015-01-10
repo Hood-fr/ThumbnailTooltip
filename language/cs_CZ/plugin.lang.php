de0765690c524d4d5dd5b38ca0b5d53dac13c8ef
TranslationTeam <translate-github@piwigo.org>
2015-01-10 07:12:44 +0000

    Update cs_CZ, thanks to kjus

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['tn_separator'] = 'Oddělovač (-)';
+$lang['tn_type1'] = 'Název';
+$lang['tn_type4'] = 'Popis';
+$lang['tn_configuration_saved'] = 'Nastavení uloženo';
+$lang['tn_default_parameters'] = 'Původní nastavení';
+$lang['tn_default_parameters_saved'] = 'Původní nastavení uloženo';
+$lang['tn_display_cat'] = 'Alba';
+$lang['tn_display_img'] = 'Fotografie';
+$lang['tn_type5'] = 'Autor';
