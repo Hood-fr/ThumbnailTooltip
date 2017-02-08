74e99a84b3327f040019f79b33ced3824ab20814
Sarybe <sarybe@gmail.com>
2017-02-08 23:01:53 +0100

    Converting line breaks

--- maintain.inc.php
+++ maintain.inc.php
@@ -1,36 +1,17 @@
-<?php
-
-if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
-
-function plugin_install()
-{
-  include(dirname(__FILE__).'/admin/config_default.inc.php');
-
-  $query = '
-INSERT INTO ' . CONFIG_TABLE . ' (param,value,comment)
-VALUES ("thumbnail_tooltip" , "'.addslashes(serialize($config_default)).'" , "Thumbnail Tooltip plugin parameters");';
-  pwg_query($query);
-
-  $query = 'SHOW FULL COLUMNS FROM ' . HISTORY_TABLE . ';';
-  $result = array_from_query($query, 'Field');
-  if (!in_array('thumbnail_tooltip', $result))
-  {
-    pwg_query('ALTER TABLE '.HISTORY_TABLE.' ADD `thumbnail_tooltip` ENUM(\'true\', \'false\') NULL DEFAULT NULL');
-  }
-}
-
-function plugin_uninstall()
-{
-  $query = 'DELETE FROM ' . CONFIG_TABLE . ' WHERE param="thumbnail_tooltip" LIMIT 1;';
-  pwg_query($query);
-
-  $query = 'SHOW FULL COLUMNS FROM ' . HISTORY_TABLE . ';';
-  $result = array_from_query($query, 'Field');
-  if (in_array('thumbnail_tooltip', $result))
-  {
-    $q = ' ALTER TABLE '.HISTORY_TABLE.' DROP `thumbnail_tooltip`';
-    pwg_query( $q );
-  }
-}
-
+<?php
+if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
+
+function plugin_install() {
+  include(dirname(__FILE__).'/admin/config_default.inc.php');
+
+  $query = 'INSERT INTO ' . CONFIG_TABLE . ' (param,value,comment) VALUES ("thumbnail_tooltip" , "'.addslashes(serialize($config_default)).'" , "Thumbnail Tooltip plugin parameters");';
+
+  pwg_query($query);
+}
+
+
+function plugin_uninstall() {
+  $query = 'DELETE FROM ' . CONFIG_TABLE . ' WHERE param="thumbnail_tooltip" LIMIT 1;';
+  pwg_query($query);
+}
 ?>
